2ed027bf1ecddd21583aac1aefe6086fce5776e4
vpadua <vgcpad@gmail.com>
2024-09-24 13:15:17 -0400

    created searchContact() function

--- contact_book.py
+++ contact_book.py
@@ -1,23 +1,53 @@
 ### Contact book
 ### An application that allows users to add, search, and delete contacts.
 
-'''
-class Contact:
-    def __init__(self, firstName, lastName, phoneNumber):
-        self.firstName = firstName
-        self.lastName = lastName
-        self.phoneNumber = phoneNumber
-'''
+import time
 
-contactBook = {}
 
-def newContact():
-    contactName = input('Enter first and last name of contact: ').title() 
-    phoneNumber = input('Enter phone number: ')
+contactBook = {'John Jo': '555-555-5555', 'Mugi Mu': '556-556-5656', 'Mike Mi' : '554-554-5454', 'Lora Lo' : '557-557-5757'}
+
+def addContact():
+    contactName = input('\nEnter first and last name of contact: ').title() 
+    phoneNumber = input('\nEnter phone number: ')
     contactBook.update([(contactName, phoneNumber)])
-    print ("Contact has been added")
+    print ("\n\nContact has been added\n\n\n")
 
-newContact()
-print (contactBook)
+def searchContact():
+    search = input("\nEnter name of contact: ").title()
+    if search in contactBook:
+        print(f"Contact found: {search}, Phone Number: {contactBook[search]}\n\n\n")
 
+    else:
+        contactFound = False
+        for name, phone in contactBook.items():
+            if search in name:
+                print(f"\nContact found: {name}, Phone Number: {phone}\n\n\n")
+                contactFound = True
+                break
+        if not contactFound:
+                print ("\n\nContact not found\n")
+
+
+
+print ("\n\nContact Book\n")
+
+while True:
+
+    for name, phone in contactBook.items():
+        print (f"{name}: {phone}")
+
+    action = input("\n\n(To exit type exit)\nWould you like to add, search, or delete a contact? ").upper()
+
+    
+    if 'ADD' in action:
+        addContact()
+    elif 'SEARCH' in action:
+        searchContact()
+
+    '''
+    elif 'DELETE' in action:
+
+    else:
+        quit()
+    '''
 
